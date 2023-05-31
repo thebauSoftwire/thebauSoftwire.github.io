19eb088202ff32b388306d57f5c0c1140dc1371f
thebauSoftwire <thea.bautista@softwire.com>
2023-05-31 11:39:01 +0100

    fixing cards

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function showRecipeDescription(id) {
         downArrowButton.style.button = "flex";
     }
 }
+
+function hideRecipeDescription(id) {
+    const recipeDescription = document.getElementById(id);
+    const downArrowButton = document.getElementById(`${id}-down-button`);
+    const upArrowButton = document.getElementById(`${id}-up-button`);
+
+    if (recipeDescription.style.display === "flex") {
+        recipeDescription.style.display = "none";
+        upArrowButton.style.display = "none";
+        downArrowButton.style.display = "flex";
+    } else {
+        recipeDescription.style.display = "flex";
+        upArrowButton.style.button = "flex";
+        downArrowButton.style.button = "none";
+    }
+}
